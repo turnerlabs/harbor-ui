03d2748d420fe53c8e7d7a093d21fba0a25c234a
Wilson Wise <wilson.wise@turner.com>
2018-01-24 11:11:15 -0500

    fix(rss): only need one fail handler

--- app.js
+++ app.js
@@ -219,11 +219,6 @@ app.get('/api/v1/blog-feed', function(req, res) {
             'Content-Type': 'application/rss+xml'
         }).status(response.code).send(response.body);
         logInfo(res);
-    }).fail(function (response) {
-        res.set({
-            'Content-Type': 'application/rss+xml'
-        }).status(response.code).send(response.body);
-        logInfo(res);
     });
 });
 
